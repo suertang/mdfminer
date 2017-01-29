adfe9e48c81648adc3d01cca57138d67175314bd
Patrick Menschel <menschel.p@posteo.de>
2017-01-29 11:16:40 +0100

    issue the first real project version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
+    version='0.0.1',
 
     description='A data miner for Measure Data File Format (.mdf)',
     long_description=long_description,
