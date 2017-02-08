b3d0f539549a4d1dda075aebd09f03b90a6f8a26
Patrick Menschel <menschel.p@posteo.de>
2017-02-08 18:07:46 +0100

    update version, remove built-in dependencies, add mdftools to __init__.py

--- mdfminer/__init__.py
+++ mdfminer/__init__.py
@@ -1,1 +1,2 @@
 from .mdf import *
+from .mdftools import *
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',
+    version='0.0.2',
 
     description='A data miner for Measure Data File Format (.mdf)',
     long_description=long_description,
@@ -81,7 +81,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['optparse','struct','datetime'],
+    install_requires=[],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
